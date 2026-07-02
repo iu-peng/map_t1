46539462e8357bdf79253107f82159d06356be07
iu-peng <iu_peng@163.com>
2026-07-02 19:45:30 +0800

    feat: render navigation app

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
-console.log(createRoot, App);
+const el = window['document']['getElementById']('root');
+
+if (el) {
+  createRoot(el).render(<App />);
+}
